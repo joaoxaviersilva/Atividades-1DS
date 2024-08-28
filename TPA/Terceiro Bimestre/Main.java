8ab1970117dfe991903f488d8c13a2dc9015b90a
João Xavier <166918086+joaoxaviersilva@users.noreply.github.com>
2024-08-28 10:51:07 -0300

    28/08/2024
    Exercício 2

--- TPA/Terceiro Bimestre/Main.java
+++ TPA/Terceiro Bimestre/Main.java
@@ -1,8 +1,8 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-package exercicio1;
+package exercicio2;
 
 import java.util.Scanner;
 
@@ -11,92 +11,27 @@ import java.util.Scanner;
  * @author r1spo
  */
 public class Main {
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite a hora do início de seu turno: ");
-      int hora = sc.nextInt();
-      switch(hora){
-          case 0:
-               System.out.println("Seu turno é Noite"); 
-               break;
-          case 1:
-               System.out.println("Seu turno é Noite"); 
-                break;
-          case 2:
-               System.out.println("Seu turno é Noite"); 
-               break;
-          case 3:
-               System.out.println("Seu turno é Noite"); 
-               break;
-          case 4:
-              System.out.println("Seu turno é Noite");
-              break;
-          case 5:
-              System.out.println("Seu turno é Manhã");
-              break;
-          case 6:
-               System.out.println("Seu turno é Manhã");
-               break;
-          case 7:
-               System.out.println("Seu turno é Manhã");
-               break;
-          case 8:
-               System.out.println("Seu turno é Manhã");
-               break;
-          case 9:
-               System.out.println("Seu turno é Manhã");
-               break;
-          case 10:
-               System.out.println("Seu turno é Manhã");
-               break;
-          case 11:
-               System.out.println("Seu turno é Manhã");
-               break;
-          case 12:
-               System.out.println("Seu turno é Manhã");
-               break;
-          case 13:
-              System.out.println("Seu turno é Tarde");
-              break;
-          case 14:
-               System.out.println("Seu turno é Tarde");
-               break;
-          case 15:
-               System.out.println("Seu turno é Tarde");
-               break;
-          case 16:
-               System.out.println("Seu turno é Tarde");
-               break;
-          case 17:
-               System.out.println("Seu turno é Tarde");
-               break;
-          case 18:
-               System.out.println("Seu turno é Tarde");
-               break;
-          case 19:
-               System.out.println("Seu turno é Tarde");
-               break;
-          case 20:
-               System.out.println("Seu turno é Tarde");
-               break;
-          case 21:
-               System.out.println("Seu turno é Noite"); 
-               break;
-          case 22:
-               System.out.println("Seu turno é Noite"); 
-               break;
-          case 23:
-               System.out.println("Seu turno é Noite"); 
-               break;
-          default:
-              System.out.println("Não existe outro horário");
-                
-                
-                
-                
-                
-                }
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Digite sua altura em metros: ");
+        double a = sc.nextDouble();
+        System.out.println("Digite seu peso em quilogramas");
+        double p = sc.nextDouble();
+        double imc = p/(a*a);
+        if(imc < 18){
+            System.out.println("Seu IMC é: " + imc + "." + " Classicado como: Magreza");
+        }
+        else if(imc >= 18 && imc <=24.9){
+            System.out.println("Seu IMC é: " + imc + "." + " Classicado como: Saudável");
+        }
+        else if(imc >= 25 && imc <= 29.9){
+            System.out.println("Seu IMC é: " + imc + "." + " Classicado como: Sobrepeso");
+        }
+        else if(imc >= 30){
+            System.out.println("Seu IMC é: " + imc + "." + " Classicado como: Obesidade");
+        }
+        else{
+            System.out.println("Algo deu errado!");
+        }
     }
-    }
-
+}
